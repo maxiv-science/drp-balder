917ef21c99c7c8c0ba350ba7926cbda438161c15
Michele Cascella <michele.cascella@maxiv.lu.se>
2024-09-27 17:21:07 +0200

    trying to add a timer

--- src/reducer.py
+++ src/reducer.py
@@ -1,5 +1,6 @@
 import logging
 import h5py
+import h5pyd
 import os
 import numpy as np
 
@@ -9,6 +10,9 @@ logger = logging.getLogger(__name__)
 
 class BalderReducer:
     def __init__(self, *args, **kwargs):
+        self.hsds = h5pyd.File("http://balder-pipeline-hsds.daq.maxiv.lu.se/home/live", username="admin",
+                               password="admin", mode="a")
+
         self.publish = {"map": {}, "control":{}}
         self.projections = []
         self._fh = None
@@ -45,6 +49,31 @@ class BalderReducer:
             oldsize = self._roi_dset.shape[0]
             self._roi_dset.resize(max(1 + result.event_number, oldsize), axis=0)
             self._roi_dset[result.event_number-1] = result.payload.roi_sum
+
+
+    def timer(self):
+        print("timed")
+        if len(self.buffer) > 0:
+            if self.first:
+                self.hsds.require_group("pcap")
+                try:
+                    del self.hsds["pcap"]["data"]
+                except:
+                    pass
+                self.hsds["pcap"].require_dataset("data", shape=(0,), maxshape=(None,),
+                                                  dtype=self.ds_dt)  # len(result.payload["pcap_start"]),
+
+                self.first = False
+
+            cpy = copy(self.buffer)
+            self.buffer = []
+            print("upload buffer", cpy)
+            oldsize = self.hsds["pcap/data"].shape[0]
+            print(oldsize)
+            self.hsds["pcap/data"].resize(oldsize + len(cpy), axis=0)
+            self.hsds["pcap/data"][oldsize:oldsize+len(cpy)] = cpy
+
+        return 1
 
 
     def finish(self, parameters=None):
