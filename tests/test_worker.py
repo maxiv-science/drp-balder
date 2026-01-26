f863d842bfac241f52bd7f365804515edc0f840f
Michele Cascella <michele.cascella@maxiv.lu.se>
2026-01-26 10:55:01 +0100

    replace test value (algo changed)

--- tests/test_worker.py
+++ tests/test_worker.py
@@ -52,7 +52,7 @@ def test_livequery() -> None:
     assert f["xes/roi_sum/data"].shape == (41,)
     assert f["xes/proj_corrected/frame"].shape == (41, 1030)
     assert f["xes/roi_sum/data"][17] == 102
-    assert f["xes/band_2theta/data"][41] == 1
+    assert abs(f["xes/band_2theta/data"][41] - 3.759838) < 3.759e-2
     assert f["xes/band_theta/data"][1] == 34
     stop_event.set()
 
